dafa4c5000a18e51c0ac690dadff093d8f414d2a
ganishm <ganishronl@gmail.com>
2024-01-31 10:54:33 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Login />} />
           <Route
             path="/dashboard"
             element={
