86aa960255d6e4dce602598da208c4a773a66ee7
ganishm <ganishronl@gmail.com>
2024-01-31 12:10:28 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route
             path="/dashboard"
             element={
